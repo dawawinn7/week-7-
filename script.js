9836a9156308dd3975a9c20c4aeeb51096c8160f
dawawinn7 <143560740+dawawinn7@users.noreply.github.com>
2024-03-08 16:11:24 +0000

    updated march 8

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
 // script.js - Let's add some interactivity to make our web development journey exciting!
 console.log("Hello World");
-alert ("hello world") ;
+//alert("hello world") ;
+
+let firstVariable =  "this is a string";
+console.log(firstVariable);
+//alert(firstVariable);
+console.log(firstVariable+"!");
+
+let exPoint = "!"; 
+console.log(firstVariable + exPoint);
+
+let myFirstInt = 1;
+console.log(myFirstInt);
+console.log(myFirstInt+ 5);
+console.log(myFirstInt * 100) ;
+console.log(myFirstInt+exPoint);
+
+let mySecondInt = 760;
+console.log(mySecondInt % 2);
+console.log(mySecondInt % 11);
+console.log("division");
+console.log(mySecondInt / 11);
+
+console.log("increment");
+console.log(++mySecondInt );
+console.log("decriment");
+console.log(--mySecondInt);
+
+let hasRemainder = 100 % 7 > 0 ;
+console.log(hasRemainder);
+
+//alert(hasRemainder);
+if(50 % 5 == 0){
+console.log("5 is a multiple of 50")
+}
+else{
+    console.log("5 is not a multiple of 50");
+}
+if (mySecondInt % 3 == 0) {
+    console.log(mySecondInt + "is a multiple of 3");
+}
+else {
+    console.log(mySecondInt + "is not a multiple of 3");
+}
+
+
+
